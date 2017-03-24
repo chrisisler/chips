be6ff4d181164e83a0c2c07cb71b6201341d3772
Chris Isler <christopherisler1@gmail.com>
2017-03-24 03:36:52 -0400

    removed native wraps from index.js. added API in README. updated test/curry

--- test/majority.js
+++ test/majority.js
@@ -5,27 +5,18 @@ describe('majority', function() {
     var isEven = function(x) { return x % 2 === 0; };
     var isOdd = function(x) { return x % 2 === 1; };
     var atLeastFive = function(x) { return x >= 5; };
-    var isNumber = function(x) { return C.is('Number', x); };
+    var isNumber = function(x) { return Object.prototype.toString.call(x) === '[object Number]'; };
 
     var nums = [ 1, 3, 5, 7, 9 ];
 
-    it('Returns true if all predicates are satisfied by at least half of the values', function() {
-        assert.strictEqual(
-            C.majority([ isNumber, atLeastFive ], nums),
-            true
-        );
-        assert.strictEqual(
-            C.majority([ isEven, isNumber ], [ 2, 4, 'foo', 7, 8 ]),
-            true
-        );
+    it('returns true if all predicates are satisfied by at least half of the values', function() {
+        assert.strictEqual(C.majority([ isNumber, atLeastFive ], nums), true);
+        assert.strictEqual(C.majority([ isEven, isNumber ], [ 2, 4, 'foo', 7, 8 ]), true);
     });
 
     it('is curried', function() {
         var majorityOddAtLeastFive = C.majority([ isOdd, atLeastFive ]);
-        assert.strictEqual(
-            majorityOddAtLeastFive(nums),
-            true
-        );
+        assert.strictEqual(majorityOddAtLeastFive(nums), true);
         assert.strictEqual(typeof majorityOddAtLeastFive, 'function');
     });
 });
